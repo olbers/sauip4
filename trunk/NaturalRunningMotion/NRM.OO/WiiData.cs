014239bb6d5f28ea4b7d28b6167c60b274a0b320
helder.pestana <helder.pestana@ca793e68-2e70-11de-8ac3-93d7c2238582>
2009-05-04 03:42:34 +0000

    Checkin Inicial do projecto de VS 2008

---
+++ trunk/NaturalRunningMotion/NRM.OO/WiiData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRM.OO
+{
+    public class WiiData
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public float Value { get; set; }
+        public WiiData()
+        {
+            X = Y = Z = Value = 0;
+        }
+        public WiiData(float mX, float mY, float mZ)
+        {
+            X = mX;
+            Y = mY;
+            Z = mZ;
+            Value = CalculateValue(X, Y, Z);
+        }
+
+        private float CalculateValue(float mX, float mY, float mZ)
+        {
+            return Math.Abs(mX) + Math.Abs(mY) + Math.Abs(mZ);
+        }
+
+    }
+}
---
+++ trunk/NaturalRunningMotion/NRM.OO/WiiDataColl.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+
+namespace NRM.OO
+{
+    public class WiiDataColl:Collection<WiiData>
+    {
+    }
+}
